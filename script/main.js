77567ba76a986ad4fee39de89715128db49d97a0
Haybuka <paschal.nduleo@gmail.com>
2020-08-23 00:35:19 +0100

    Fixed footer issue

--- script/main.js
+++ script/main.js
@@ -7,31 +7,29 @@ const sliderSpan = document.querySelector('#wrapper nav .toggler span')
 const midSpan = document.querySelector('nav .toggler span:nth-child(2)')
 const firstSpan = document.querySelector('nav .toggler span:nth-child(1)')
 const thirdSpan = document.querySelector('nav .toggler span:nth-child(3)')
+
 dropContact.addEventListener('click', (n) => {
     contact.classList.toggle('open');
     drive.classList.toggle('car');
-
+    console.log(contact)
 })
 
 slider.addEventListener('click', (n) => {
+    slider.classList.toggle('rotates')
     slideUl.classList.toggle('slideIn');
     midSpan.classList.toggle('drop')
     firstSpan.classList.toggle('cross')
     thirdSpan.classList.toggle('revCross')
-
-
 })
-// sliderSpan.addEventListener('click', (n) => {
-//     slideUl.classList.toggle('slideIn');
-// })
+
 console.log('Hi there,your royal Geekiness.....Try Not to Break Anything!!!!!....Just Kidding')
 AOS.init();
 
 
-window.addEventListener("load", function () {
-    setTimeout(() => {
-        const preloader = document.querySelector(".lds-heart");
-        preloader.className += "loaded";
-    }, 2000);
+// window.addEventListener("load", function () {
+//     setTimeout(() => {
+//         const preloader = document.querySelector(".lds-heart");
+//         preloader.className += "loaded";
+//     }, 2000);
 
-});
+// });
